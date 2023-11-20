8f42bca3b8d4609543b83e7e8ebcee5c5071648d
Vero1806 <146030980+Vero1806@users.noreply.github.com>
2023-11-20 16:25:01 +0100

    6, 7 y 9. Cole2

--- .idea/NewPractica5.py
+++ .idea/NewPractica5.py
@@ -158,9 +158,78 @@ Ejercicio6: Vamos a pedir al usuario que introduzca un número y vamos a intenta
 las distintas excepciones que se pueden producir
 '''
 
+num = int(input("Introduce un número como divisor"))
+
+try:
+    resultado = 10 / num
+    print(resultado)
+except ValueError as e:
+    print("El número ", num, "no es un entero. Error:", e)
+except ZeroDivisionError as e:
+    print("No se puede dividir entre cero. Error:", e)
+
 '''
 Ejercicio7: Vamos a crear un diccionario (pidiendo los valores por pantalla hasta introducir *) donde las claves son los nombres de los vinos y el valor una lista con la información sobre la denominación de origen, añada y precio. Después, hagamos un programa en que, dado el diccionario de vinos y una lista con un rango de precios [menorPrecio, mayorPrecio], nos devuelva una lista de los nombres, ordenados alfabéticamente de los vinos que estén en este rango de precios.
 '''
+vinos =  {}
+lista = []
+#print(vinos)
+nombre=""
+origen=""
+añada=""
+precio=""
+
+print("Vamos a introducir los vinos en el diccionario. La ejecucion se parara cuando introduzcas *")
+try:
+    while True :
+        nombre = input("Introduce el nombre del vino ")
+        if nombre == "*":
+            break
+        origen = input("Introduce la denominación de origen ")
+        if origen == "*":
+            break
+        añadastring = input("Introduce el año de fabricación del vino ")
+        if añadastring == "*":
+            break
+        preciostring = input("Introduce el precio del vino.(Separa los decimales con .) ")
+        if preciostring == "*":
+            break
+
+        añada = int(añadastring)
+        precio = float(preciostring)
+
+        listadatos = [origen,añada,precio]
+
+        datos = vinos.setdefault(nombre, listadatos)
+
+        if nombre in vinos:
+            print("El vino ", nombre, "ya esta en la lista")
+
+        else:
+            listadatos.append(datos)
+            print("Se han agregado los datos", listadatos, "al vino", nombre)
+
+
+    print(vinos)
+
+except ValueError as e:
+    print("No has introducido un valor valido en la añada o en el precio. Error =", e)
+
+
+
+print("Ahora vamos a buscar los vinos en el rango de precios que prefieras")
+menorPrecio = float(input("Introduce la cantidad mínima que quieras gastarte.(Separa los decimales con .)"))
+mayorPrecio = float(input("Introduce la cantidad máxima que quieras gastarte.(Separa los decimales con .)"))
+
+
+for nombre, datos_lista in vinos.items():
+    for datos in datos_lista:
+        precio_vino = datos[2]
+        if menorPrecio <= precio_vino <= mayorPrecio:
+            nombres.append(nombre)
+
+print(nombres.sort)
+
 
 '''
 Ejercicio08: Vamos a crear un programa que lea por teclado las temperaturas mínimas de los 3 primeros meses del año de distintas ciudades, de la forma dic = {'Londres': [3.4, 6.3, 10.5], 'Oslo': [-3.8, -5.0, 4.2], 'Rennes': [2.5, 3.1, 12.3]} y una vez obtenido el carácter de salida * nos devuelva:
--- .idea/prueba.py
+++ .idea/prueba.py
@@ -1,68 +1,14 @@
 '''
-Ejercicio7: Vamos a crear un diccionario (pidiendo los valores por pantalla hasta introducir *)
-donde las claves son los nombres de los vinos y el valor una lista con la información sobre la denominación de origen,
-añada y precio.
-Después, hagamos un programa en que, dado el diccionario de vinos y
-una lista con un rango de precios [menorPrecio, mayorPrecio], nos devuelva una lista de los nombres, ordenados
-alfabéticamente de los vinos que estén en este rango de precios.
+Ejercicio6: Vamos a pedir al usuario que introduzca un número y vamos a intentar dividir 10 por el número introducido manejando
+las distintas excepciones que se pueden producir
 '''
 
-vinos =  {}
-lista = []
-#print(vinos)
-nombre=""
-origen=""
-añada=""
-precio=""
+num = int(input("Introduce un número como divisor"))
 
-print("Vamos a introducir los vinos en el diccionario. La ejecucion se parara cuando introduzcas *")
 try:
-    while True :
-        nombre = input("Introduce el nombre del vino ")
-        if nombre == "*":
-            break
-        origen = input("Introduce la denominación de origen ")
-        if origen == "*":
-            break
-        añadastring = input("Introduce el año de fabricación del vino ")
-        if añadastring == "*":
-            break
-        preciostring = input("Introduce el precio del vino.(Separa los decimales con .) ")
-        if preciostring == "*":
-            break
-
-        añada = int(añadastring)
-        precio = float(preciostring)
-
-        listadatos = [origen,añada,precio]
-
-        datos = vinos.setdefault(nombre, listadatos)
-
-        if nombre in vinos:
-            print("El vino ", nombre, "ya esta en la lista")
-
-        else:
-            listadatos.append(datos)
-            print("Se han agregado los datos", listadatos, "al vino", nombre)
-
-
-    print(vinos)
-
+    resultado = 10 / num
+    print(resultado)
 except ValueError as e:
-    print("No has introducido un valor valido en la añada o en el precio. Error =", e)
-
-
-
-print("Ahora vamos a buscar los vinos en el rango de precios que prefieras")
-menorPrecio = float(input("Introduce la cantidad mínima que quieras gastarte.(Separa los decimales con .)"))
-mayorPrecio = float(input("Introduce la cantidad máxima que quieras gastarte.(Separa los decimales con .)"))
-
-
-for nombre, datos_lista in vinos.items():
-    for datos in datos_lista:
-        precio_vino = datos[2]
-        if menorPrecio <= precio_vino <= mayorPrecio:
-            nombres.append(nombre)
-
-print(nombres.sort)
-
+    print("El número ", num, "no es un entero. Error:", e)
+except ZeroDivisionError as e:
+    print("No se puede dividir entre cero. Error:", e)
