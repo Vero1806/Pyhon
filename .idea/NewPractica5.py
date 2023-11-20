705e195c92080e4d65286ab7d16e1ba5abdd6548
Vero1806 <146030980+Vero1806@users.noreply.github.com>
2023-11-20 16:49:20 +0100

    6, 7 y 9. Cole3

--- .idea/NewPractica5.py
+++ .idea/NewPractica5.py
@@ -240,7 +240,9 @@ Ejercicio08: Vamos a crear un programa que lea por teclado las temperaturas mín
 
 '''
 Ejercicio09
-Vamos a crear un registro de eventos con un diccionario donde se registre el código del evento y después la fecha y hora, el tipo de evento (que solo acepte los valores normal o crítico), y el nombre). Después, mostrar un pequeño menú que permita al usuario:
+Vamos a crear un registro de eventos con un diccionario donde se registre el código del evento y 
+después la fecha y hora, el tipo de evento (que solo acepte los valores normal o crítico), y el nombre). 
+Después, mostrar un pequeño menú que permita al usuario:
  - Mostrar todos los eventos ordenados por fecha
  - Mostrar todos los eventos críticos
  - Buscar un evento: pedir el código por pantalla, imprimir toda la información y preguntarle al usuario si quiere además imprimir todos los eventos de ese día 
--- .idea/prueba.py
+++ .idea/prueba.py
@@ -1,14 +1,27 @@
 '''
-Ejercicio6: Vamos a pedir al usuario que introduzca un número y vamos a intentar dividir 10 por el número introducido manejando
-las distintas excepciones que se pueden producir
+Ejercicio08: Vamos a crear un programa que lea por teclado las temperaturas mínimas de los 3 primeros
+meses del año de distintas ciudades, de la forma dic = {'Londres': [3.4, 6.3, 10.5],
+'Oslo': [-3.8, -5.0, 4.2], 'Rennes': [2.5, 3.1, 12.3]} y una vez obtenido el carácter de salida * nos devuelva:
+- El valor medio de las temperaturas de cada ciudad (redondear a 2 decimales).
+- El valor mínimo de las temperaturas de cada ciudad
+- Una lista con la ciudad con la temperatura más baja y su valor
 '''
+temperaturas = {}
 
-num = int(input("Introduce un número como divisor"))
+while True:
+    ciudad = input("Introduce la ciudad de las temperaturas que vamos a comparar ")
+    if nombre == "*":
+        break
+    valor1 = float(input("Temperatura minima de enero "))
+    if nombre == "*":
+        break
+    valor2 = float(input("Temperatura mínima de febrero "))
+    if nombre == "*":
+        break
+    valor3 = float(input("Temperatura mínima de marzo "))
+    if nombre == "*":
+        break
 
-try:
-    resultado = 10 / num
-    print(resultado)
-except ValueError as e:
-    print("El número ", num, "no es un entero. Error:", e)
-except ZeroDivisionError as e:
-    print("No se puede dividir entre cero. Error:", e)
+temperaturas[ciudad]=(valor1,valor2,valor3)
+
+print(temperaturas)
