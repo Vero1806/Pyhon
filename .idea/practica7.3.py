560aee89db7616d027d3de786cb0e39f0932deef
Vero1806 <146030980+Vero1806@users.noreply.github.com>
2023-11-23 12:51:04 +0100

    practica7.3 avance

--- .idea/practica7.3.py
+++ .idea/practica7.3.py
@@ -4,6 +4,10 @@
 # Datos de salida: dos números
 
 def Intercambiar (n1,n2):
+    if n1 > n2:
+        return n1,n2
+    if n2 > n1:
+        return n2,n1
 
 
 # Función CalcularMCD: Recibe dos números y devuelve el MCD utilizando el método
@@ -15,12 +19,35 @@ def Intercambiar (n1,n2):
 # Parámetros de entrada: dos números
 # Dato devuelto: El MCD
 
+def CalcularMCD (num1, num2):
+   n1, n2 = Intercambiar(num1,num2)
+   resto = n1%n2
+
+   if resto == 0:
+       return n2
+
+   if resto != 0:
+
+    while resto != 0:
+        n1 = n2 #Divisor para a ser dividendo
+        n2 = resto #Resto pasa a ser divisor
+        resto = n1 % n2 #repetimos operación
+    return n2 #Para cuando resto = 0 el divisor que tengamos en ese momento sera MCD
+
+
+# print(CalcularMCD(39,150)) #Prueba
 
 
 # Función LeerFracion: Lee por teclado una fracción (numerador y denominador)
 #  y lo devuelve como parámetro de entrada y salida.
 # Esta función usa SimplificarFraccion para simplificar la fracción leída.
 # Datos devueltos: numerador y denominador
+
+def LeerFracion():
+  num1 = int(input("Introduce el numerador de la fracción. Debe ser un número entero "))
+  num2 = int(input("Introduce el denominador de la fracción. Debe ser un número entero "))
+  n1, n2 = SimplificarFracion(num1,num2)
+  return n1,n2
 
 
 # Función SimplificarFracion: Recibe una fracción (numerador y denominador)
@@ -29,6 +56,15 @@ def Intercambiar (n1,n2):
 # y denominador.
 # Datos devueltos: numerador y denominador
 
+def SimplificarFracion (n1,n2):
+    mcd = CalcularMCD(n1,n2)
+    num1 = n1/mcd
+    num2 = n2/mcd
+    return num1,num2
+
+
+# print(LeerFraccion()) #comprobación
+# print(SimplificarFracion(39,150)) #comprobación
 
 
 # Función EscribirFracion: Recibe una fracción (numerador y denominador)
@@ -36,6 +72,17 @@ def Intercambiar (n1,n2):
 # denominador es 1 sólo muestra el numerador.
 # Parámetros de entrada: numerador y denominador
 
+def EscribirFracion(numerador, denominador):
+
+    if denominador == 1:
+        return print(numerador)
+    if denominador == 0:
+        return print("Error el denominador de una fracción no puede ser 0")
+    else:
+        return print(numerador,"/",denominador)
+
+
+#print(EscribirFracion(39,150))
 
 
 # Función SumarFracciones: Recibe dos fracciones (numerador y denominador)
@@ -79,3 +126,4 @@ def Intercambiar (n1,n2):
 
 # Crear un programa que utilizando las funciones anteriores muestre un menú para
 # operar con fracciones.
+
