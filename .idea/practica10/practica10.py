b6445be87b58876ed5c113fd53ad4f99f4c5f3f8
Vero1806 <146030980+Vero1806@users.noreply.github.com>
2023-12-12 18:43:36 +0100

    practica 8 a medias. Terminar el 1 con el enlace dado

---
+++ .idea/practica10/practica10.py
@@ -0,0 +1,79 @@
+class Autor:
+    def __init__(self,nombreAutor, paisAutor):
+        self.__nombreAutor = nombreAutor
+        self.__paisAutor = paisAutor
+
+    @property
+    def nombreAutor(self):
+        return self.__nombreAutor
+
+    @nombreAutor.setter
+    def nombreAutor(self,nombreAutorNuevo):
+        self.__nombreAutor=nombreAutorNuevo
+
+    @property
+    def paisAutor(self):
+        return __nombreAutor
+
+    @paisAutor.setter
+    def paisAutor(self,paisAutorNuevo):
+        self.__paisAutor=paisAutorNuevo
+
+    def mostrarAutor(self):
+        return print(self.__nombreAutor,self.__paisAutor)
+
+
+
+class Libro:
+    def __init__(self,titulo,autor,anio):
+
+        self.__titulo = titulo
+        super().__init__(nombreAutor, paisAutor)
+        self.__anio = anio
+
+
+    @property
+    def titulo(self):
+        return __titulo
+
+    @titulo.setter
+    def titulo(self,tituloNuevo):
+        self.__titulo=tituloNuevo
+
+    @property
+    def autor(self):
+        return self.__autor
+
+    @autor.setter
+    def autor(self, nuevo_autor):
+        self.__autor = nuevo_autor
+
+    @property
+    def anio(self):
+        return __anio
+
+    @anio.setter
+    def anio(self,anioNuevo):
+        self.__anio=anioNuevo
+
+    def mostrarLibro(self):
+        return print(self.__titulo,self.__autor,self.__anio)
+
+autorPedro = Autor("Pedro López", "España")
+autorAlex = Autor("Alejandro Gonzalez", "México")
+
+
+libroViaje = Libro ("Viaje al centro del mundo", autorPedro,1998)
+libroSol = Libro ("Un Sol en el centro de la Tierra", autorAlex,1945)
+
+print(autorPedro)
+print(autorAlex)
+
+print("")
+
+autorPedro.mostrarAutor()
+autorAlex.mostrarAutor()
+libroViaje.mostrarLibro()
+libroSol.mostrarLibro()
+
+
